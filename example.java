979c3c51cb48f4f3b75f084e5c7e27c92e19b888
GANGA C K <kochupotty@gmail.com>
2016-11-03 22:12:24 +0530

    a3_3

---
+++ example.java
@@ -0,0 +1,13 @@
+package a8;
+
+class example {
+	int x; int y;
+	String name;
+	public static void main(String args[])
+	{
+		example pnt=new example();
+		System.out.println("pnt is "+ pnt.name+" "+pnt.x+" "+pnt.y);
+		
+	}
+
+}
